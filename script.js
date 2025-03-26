8387626e91f33702a9b37843c3a050f29d8287ab
THAYSANTOS13 <153189367+THAYSANTOS13@users.noreply.github.com>
2025-03-26 14:55:03 -0300

    .

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function (){
+ let tamanhoAtualFonte= 1;
+ const aumentaFonteBotao= document.getElementById('aumentar-fonte');
+ 
+ aumentaFonteBotao.addEventListener ('click', function (){
+    tamanhoAtualFonte += 0.1;
+    document.DOCUMENT_TYPE_NODE.style.fontSize= '${tamanhoAtualFonte}rem';
+ })
+ const diminuirFonteBotao= document.getElementById('diminuir-fonte');
+ tamanhoAtualFonte-= 0.1;
+ document.body.style.fontSize= '${tamanhoAtualFonte}rem';
+})
